ac1c6bf1fe6b2ca640120759a043fd1d817b00f7
soupaul <soupaul@expediagroup.com>
2022-07-15 00:45:27 +0530

    timer added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var port = 8080;
+const hostname = "0.0.0.0"
 var app = express();
 const db = require('./config/mongoose');
 const expressLayouts = require('express-ejs-layouts');
+const axios = require('axios');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -12,9 +14,12 @@ app.set('layout extractScripts', true);
 app.use(express.static('./assets'));
 app.use('/', require('./routes'));
 
-app.listen(port, function(err) {
+app.listen(port, hostname, function(err) {
     console.log('server is running');
+    setInterval(function () {
+        axios.get('http://localhost:8080/createDatabase');
+    }, 86400000);
     if (err) {
         console.log(err);
     }
-})
+});
